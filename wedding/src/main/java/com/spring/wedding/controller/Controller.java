5994b420a31404cecca57a798ed8e140b4d2674a
papkjh1 <papkjh1@naver.com>
2021-04-08 01:40:46 +0900

    컨트롤러 작성중

--- wedding/src/main/java/com/spring/wedding/controller/Controller.java
+++ wedding/src/main/java/com/spring/wedding/controller/Controller.java
@@ -1,5 +1,12 @@
 package com.spring.wedding.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.spring.wedding.service.User_infoService;
+
+@org.springframework.stereotype.Controller
 public class Controller {
 
+		@Autowired
+		private User_infoService us;
 }
